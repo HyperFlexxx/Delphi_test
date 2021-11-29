d96f086ff3de40f94e48f5f96033ef3bd8c2837f
skuraew@yandex.ru <HyperFlexxx>
2021-11-30 00:45:50 +0900

    just for fun

---
+++ test/Unit1.pas
@@ -0,0 +1,35 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Label1.Text:=Edit1.Text;
+end;
+
+end.
