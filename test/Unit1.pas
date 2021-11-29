f1b83a530da5b9353bae1628f6bbc9e766b47b8c
skuraew@yandex.ru <HyperFlexxx>
2021-11-30 01:25:27 +0900

    New fun

--- test/Unit1.pas
+++ test/Unit1.pas
@@ -5,14 +5,18 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.Edit;
+  FMX.Controls.Presentation, FMX.Edit, FMX.Effects, FMX.Ani, FMX.MagnifierGlass,
+  FMX.DateTimeCtrls;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
     Edit1: TEdit;
     Label1: TLabel;
+    Panel1: TPanel;
+    DateEdit1: TDateEdit;
     procedure Button1Click(Sender: TObject);
+
   private
     { Private declarations }
   public
@@ -27,9 +31,11 @@ implementation
 {$R *.fmx}
 
 
+
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   Label1.Text:=Edit1.Text;
 end;
+
 
 end.
